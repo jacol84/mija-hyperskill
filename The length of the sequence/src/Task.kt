f0f5d53c263ec0d88a6abf0cb046c610ababc1c8
jac <devjac@ogarnij.se>
2020-07-13 20:58:25 +0200

    The length of the sequence

---
+++ The length of the sequence/src/Task.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    var i = 0
+    while (scanner.nextInt() != 0) {
+        i++
+    }
+    print(i)
+}
