93f000eb9825ccbe8e40adc9fd491950b72518b2
jac <devjac@ogarnij.se>
2020-08-06 19:33:07 +0200

    - equals() check version 2

--- equals() check/src/Task.kt
+++ equals() check/src/Task.kt
@@ -1,21 +1,12 @@
-import java.util.Scanner
+import java.util.*
 
-data class Client(val name: String, val age: Int, val balance: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        if (javaClass != other?.javaClass) {
-            return false
-        }
-        other as Client
-        return name == other.name && age == other.age
-    }
+data class Client(val name: String, val age: Int) {
+    var balance: Int = 0
 }
 
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
-    val client1 = Client(input.next(), input.nextInt(), input.nextInt())
-    val client2 = Client(input.next(), input.nextInt(), input.nextInt())
+    val client1 = Client(input.next(), input.nextInt()).also { it.balance = input.nextInt() }
+    val client2 = Client(input.next(), input.nextInt()).also { it.balance = input.nextInt() }
     print(client1 == client2)
 }
